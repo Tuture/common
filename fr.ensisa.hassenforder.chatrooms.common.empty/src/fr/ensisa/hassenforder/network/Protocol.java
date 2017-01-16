b3bd585999dbd0396b95d860aa12d9f898c2ebdd
e1502061 <e1502061@VALERIA.uha.fr>
2017-01-16 13:52:29 +0100

    Adding new Protocol for : - disconnection - unsuscription - approbation - sending message

--- fr.ensisa.hassenforder.chatrooms.common.empty/src/fr/ensisa/hassenforder/network/Protocol.java
+++ fr.ensisa.hassenforder.chatrooms.common.empty/src/fr/ensisa/hassenforder/network/Protocol.java
@@ -7,14 +7,19 @@ public interface Protocol {
 	
 	public static final int RP_OK		= 1;
 	public static final int RP_KO		= 2;
-	public static final int RP_CHANNELS 	= 6;
-	public static final int RP_LOAD = 7;
+	public static final int RP_CHANNELS = 3;
+	public static final int RP_LOAD 	= 4;
 	
 	
 	
-	public static final int RQ_CONNECT	= 3;
-	public static final int RQ_CHANNEL  = 4;
-	public static final int RQ_LOAD 	= 5;
+	public static final int RQ_CONNECT		 = 5;
+	public static final int RQ_CHANNEL  	 = 6;
+	public static final int RQ_LOAD 		 = 7;
+	public static final int  RQ_DISCONNECT 	 = 8; 
+	public static final int  RQ_UNSUSCRIBE 	 = 9;
+	public static final int  RQ_SENTMESSAGE  = 10;
+	public static final int  RQ_APPROBATION  = 11; // Valider / Invalider un message dans un channel modéré
+	public static final int  RQ_SEND_MESSAGE = 12;
 	
 	
 	/*public static final int RQ_CREATE_ROOM 		= 11;
@@ -23,6 +28,5 @@ public interface Protocol {
 	public static final int RQ_SEND_MESSAGE		= 7;
 	public static final int RP_VALID 		= 8;
 	public static final int REQ_SUSCRIBE  		= 9;
-	public static final int RP_ROOM_MESSAGE 		= 10;
-*/
+	public static final int RP_ROOM_MESSAGE 		= 10;*/
 }
